fae9360810402fe021b883a4f3fe00e258b0bf2e
Sebastián Ariel Szperling <sebastian.szperling@despegar.com>
2017-06-28 22:34:38 -0300

    Re-add stetho dependency

--- app/src/main/java/zebaszp/phoneinfo/application/Application.kt
+++ app/src/main/java/zebaszp/phoneinfo/application/Application.kt
@@ -1,13 +1,11 @@
 package zebaszp.phoneinfo.application
 
-import com.facebook.soloader.SoLoader
 import com.facebook.stetho.Stetho
 
 class Application : android.app.Application() {
 
     override fun onCreate() {
         super.onCreate()
-        SoLoader.init(this, false)
         Stetho.initializeWithDefaults(this)
     }
 }
