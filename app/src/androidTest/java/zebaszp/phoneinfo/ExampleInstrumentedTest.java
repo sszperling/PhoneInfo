b2e77de2e39f04a928cb972a39426a1a7e0020e9
Sebastián Szperling <sebastian.szperling@despegar.com>
2020-09-19 17:41:29 -0300

    Modernize: use androidx instead of Support libs

--- app/src/androidTest/java/zebaszp/phoneinfo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/zebaszp/phoneinfo/ExampleInstrumentedTest.java
@@ -1,8 +1,9 @@
 package zebaszp.phoneinfo;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,7 +21,7 @@ public class ExampleInstrumentedTest {
 	@Test
 	public void useAppContext() {
 		// Context of the app under test.
-		Context appContext = InstrumentationRegistry.getTargetContext();
+		Context appContext = ApplicationProvider.getApplicationContext();
 		assertEquals("zebaszp.phoneinfo", appContext.getPackageName());
 	}
 }
--- app/src/main/java/zebaszp/phoneinfo/ui/main/MainActivity.java
+++ app/src/main/java/zebaszp/phoneinfo/ui/main/MainActivity.java
@@ -2,13 +2,14 @@ package zebaszp.phoneinfo.ui.main;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.facebook.device.yearclass.YearClass;
 
